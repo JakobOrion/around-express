d5bb270b00e705133b70e608ec18f0c865739ffa
Jakob Pfefferkorn <jakoborion@gmail.com>
2021-03-30 01:44:51 -0400

    setup project folders and files, create routes for cards and users, 404 working

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 
 const { PORT = 3000 } = process.env;
+const cardsRoute = require('./routes/cards');
+const userRoute = require('./routes/users');
 
 const app = express();
 
-//test
-app.listen(PORT, () => {
-  console.log('App listening at port ${PORT}')
-})
+app.listen(PORT);
+
+app.use('/cards', cardsRoute);
+app.use('/users', userRoute);
+
+app.use((req, res, next) => {
+  res.status(404).send({ message: 'Requested resource not found' });
+  next();
+});
