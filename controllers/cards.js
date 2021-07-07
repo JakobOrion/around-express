5e7b8dd14eabd7dce65e4da0f55018a28897b062
Jakob Pfefferkorn <jakoborion@gmail.com>
2021-07-06 20:33:53 -0400

    dev: move error check to utils, update cards controller

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,31 +1,33 @@
 const Card = require('../models/card');
+const { checkErrors } = require('../utils/utils');
 
 const getCards = (req, res) => {
   Card.find({})
+    .orFail(new Error('Not Found'))
     .then((cards) => {
       res.status(200).send({ data: cards });
     })
-    .catch(() => {
-      res.status(500).send({ message: 'Requested resource not found' });
+    .catch((err) => {
+      checkErrors({ res, err });
     });
 };
 
 const createCard = (req, res) => {
   const { name, link } = req.body;
-  console.log(req.user._id);
 
   Card.create({ name, link, owner: req.user._id })
     .then((card) => res.send({ data: card }))
-    .catch(() => {
-      res.status(500).send({ message: 'Requested resource not found' });
+    .catch((err) => {
+      checkErrors({ res, err });
     });
 };
 
 const deleteCard = (req, res) => {
   Card.findByIdAndDelete(req.params.cardId)
+    .orFail(new Error('Not Found'))
     .then(() => res.status(200).send({ message: 'Card deleted' }))
-    .catch(() => {
-      res.status(500).send({ message: 'Requested resource not found' });
+    .catch((err) => {
+      checkErrors({ res, err });
     });
 };
 
